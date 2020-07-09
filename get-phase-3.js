f921626e12be346dba31b163c5aeb8817e918f32
STRNEOH <strneoh@localhost.localdomain>
2020-07-09 13:53:34 -0300

    update get phase 3  html

--- get-phase-3.js
+++ get-phase-3.js
@@ -5,28 +5,65 @@ module.exports = function(RED) {
 
     function getPhase3Node(config) {
         RED.nodes.createNode(this, config);
-        // this.websocket = config.websocket;
-        // this.websocketConfig = RED.nodes.getNode(this.websocket);
+        this.compare_selectA = config.compare_selectA;
+        this.maxValueA = config.maxValueA;
+        this.minValueA = config.minValueA;
+
+        this.compare_selectB = config.compare_selectB;
+        this.maxValueB = config.maxValueB;
+        this.minValueB = config.minValueB;
+
+        this.compare_selectC = config.compare_selectC;
+        this.maxValueC = config.maxValueC;
+        this.minValueC = config.minValueC;
         var node = this
-        // this.serial = config.serial;
-        // this.serialConfig = RED.nodes.getNode(this.serial);
-        // this.mapeamento = config.mapeamento
-        // node.channel_number = config.channel_number
-        // node.AC_mode = config.AC_mode === "true" ? true : false,
-        // node.scale = config.scale
-        // mapeamentoNode = RED.nodes.getNode(this.mapeamento);
-        
+
         node.on('input', function(msg, send, done) {
+            var _phaseA = {}
+            var _phaseB = {}
+            var _phaseC = {}
+            if (node.compare_selectA == "interval") {
+                _phaseA = {">=": parseFloat(node.minValueA), "<=": parseFloat(node.maxValueA)}
+            }
+            if (node.compare_selectA == "maxValue") {
+                _phaseA = {">=": null, "<=": parseFloat(node.maxValueA)}
+            }
+            if (node.compare_selectA == "minValue") {
+                _phaseA = {">=": parseFloat(node.minValueA), "<=": null}
+            }
+            
+            if (node.compare_selectB == "interval") {
+                _phaseB = {">=": parseFloat(node.minValueB), "<=": parseFloat(node.maxValueB)}
+            }
+            if (node.compare_selectB == "maxValue") {
+                _phaseB = {">=": null, "<=": parseFloat(node.maxValueB)}
+            }
+            if (node.compare_selectB == "minValue") {
+                _phaseB = {">=": parseFloat(node.minValueB), "<=": null}
+            }
+            
+            if (node.compare_selectC == "interval") {
+                _phaseC = {">=": parseFloat(node.minValueC), "<=": parseFloat(node.maxValueC)}
+            }
+            if (node.compare_selectC == "maxValue") {
+                _phaseC = {">=": null, "<=": parseFloat(node.maxValueC)}
+            }
+            if (node.compare_selectC == "minValue") {
+                _phaseC = {">=": parseFloat(node.minValueC), "<=": null}
+            }
+            var _compare = {
+                phase_degrees_A: _phaseA,
+                phase_degrees_B: _phaseB,
+                phase_degrees_C: _phaseC,
+            }
+
             var globalContext = node.context().global;
-            var exportMode = globalContext.get("exportMode");
             var currentMode = globalContext.get("currentMode");
             var command = {
                 type: "multimeter_modular_V1.0",
                 slot: 1,
                 method: "get_phase_3",
-                // channel_number: parseInt(node.channel_number),
-                // AC_mode: node.AC_mode ,
-                // scale: parseInt(node.scale) 
+                compare: _compare
             }
             var file = globalContext.get("exportFile")
             var slot = globalContext.get("slot");
@@ -34,23 +71,10 @@ module.exports = function(RED) {
             else{file.slots[slot].jig_error.push(command)}
             globalContext.set("exportFile", file);
             node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
-            // msg.payload = command
+            console.log(command)
             send(msg)
         });
     }
     RED.nodes.registerType("get-phase-3", getPhase3Node);
 
-    // RED.httpAdmin.get("/getPhase3",function(req,res) {
-    //     // console.log(mapeamentoNode)
-    //     if(mapeamentoNode){
-    //         res.json([
-    //             {value:mapeamentoNode.valuePort1, label: mapeamentoNode.labelPort1, hasValue:false},
-    //         ])
-    //     }
-    //     else{
-    //         res.json([
-    //             {label:"APW | IAPW || BPW | IBPW || CPW | ICPW", value: "0", hasValue:false},
-    //         ])
-    //     }
-    // });
 }
