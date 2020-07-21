26a7a5fb7d4a3df89bf7e0c1aaa382dba138c5cc
Ian Alves <igaps.eng17@uea.edu.br>
2020-07-21 15:04:10 -0400

    update

--- get-phase-3.js
+++ get-phase-3.js
@@ -5,6 +5,8 @@ module.exports = function(RED) {
 
     function getPhase3Node(config) {
         RED.nodes.createNode(this, config);
+        this.slot = config.slot;
+
         this.compare_selectA = config.compare_selectA;
         this.maxValueA = config.maxValueA;
         this.minValueA = config.minValueA;
@@ -60,15 +62,32 @@ module.exports = function(RED) {
             var globalContext = node.context().global;
             var currentMode = globalContext.get("currentMode");
             var command = {
-                type: "multimeter_modular_V1.0",
-                slot: 1,
+                type: "multimeter_modular_V1_0",
+                slot: parseInt(node.slot),
                 method: "get_phase_3",
-                compare: _compare
+                compare: _compare,
+                get_output: {},
             }
             var file = globalContext.get("exportFile")
             var slot = globalContext.get("slot");
-            if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
-            else{file.slots[slot].jig_error.push(command)}
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command);
+                    // file.begin.push(command);
+                }
+                else{
+                    file.slots[3].jig_test.push(command);
+                    // file.end.push(command);
+                }
+            }
             globalContext.set("exportFile", file);
             node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
             console.log(command)
